bc3637341084e47c4d9924e4461be99c55124997
malustewart <mastewart@itba.edu.ar>
2018-01-05 14:54:44 -0300

    Add files via upload

---
+++ config/textlog_config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define FONT_NAME "media/font/runescape_chat_font.ttf"
+#define FONT_SIZE 25
+#define DEFAULT_FONT_COLOR {(float)1, (float)1, (float)1, (float)0}
+#define DEFAULT_LINE_HEADER " >> "
