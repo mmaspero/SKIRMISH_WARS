96d07fbf85ec60b3c3dbeb49b9dfd093f21b6781
Matias <mpierdominici@itba.edu.ar>
2018-01-27 08:44:55 -0300

    Delete packageMakerMain.cpp

--- packageMakerMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "packageMaker.h"
-#include "packageValidator.h"
-#include <iostream>
-void main(void)
-{
-	char tst1[7] = { 0x11,0x05,'A','R','I','E','L' };
-	std::cout << nameIsValidator(tst1, 7) << std::endl;
-
-}
