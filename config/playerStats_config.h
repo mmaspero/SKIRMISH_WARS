787cd56baa3815fc2e132ebb28d8b974a000d292
malustewart <mastewart@itba.edu.ar>
2018-01-29 22:25:19 -0300

    borrar carpetas duplicadas en el lugar incorrecto

--- config/playerStats_config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define COIN_SPRITE_FILE "media/img/moneySprite.png"
-#define ARMY_SPRITE_FILE "media/img/armySprite.png"
-#define TIME_SPRITE_FILE "media/img/timeSprite.png"
-#define FONT_SIZE 20
-#define FONT_NAME	"media/font/ttf.ttf"
-
--- config/textlog_config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define FONT_NAME "media/font/runescape_chat_font.ttf"
-#define FONT_SIZE 25
-#define DEFAULT_FONT_COLOR {(float)1, (float)1, (float)1, (float)0}
-#define DEFAULT_LINE_HEADER " >> "
